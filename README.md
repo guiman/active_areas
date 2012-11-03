e8dc137567058c23a2fd089b124336628374c37a
Alvaro F. Lara <alvarola@gmail.com>
2012-11-02 23:01:37 -0300

    Firt commit

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
-active_areas
+activeAreas
 ============
 
 Javascript library to define sensible areas on the browser
+
+What is it about?
+-----------------
+
+A simple yet helpful library to define sensible areas on the browser that respond to position information. Basically the idea is to determin if a certain soure of position information (like the mouse), is inside an area, and then triggering some callback.
+
+But how does it work?
+---------------------
+
+Well, you need three things: a list of active areas and a list of associated callbacks. If a point is inside the area, the callback will be triggered. Check out the example on test.html, there you could see it in action, just move the mouse over the blue or red stripes and le the magic amaze you.
+
+Author
+------
+
+Le me, [@guiman](https://github.com/guiman). This is part of another project i'm currently working on: [Baku] (https://github.com/guiman/baku). A plataform that allows you to manipulate in a more human way your interactions with the browser using special but simple hardware. 
---
+++ active_areas.js
@@ -0,0 +1,86 @@
+/*
+Active area JS
+-----------------------
+
+This library allows you to define areas on the viewport
+and respond to mouse movement over them. 
+
+*/
+var activeArea = function(){
+  /*
+  
+  References:
+  -----------
+  
+  = Area should be something like [H,W,P] where:
+
+  H = height, numeric
+  W = width, numeric
+  P = point, array [x,y] representing the upper left corner
+
+  = Stream data expected is: [[X,Y,Z], [X,Y,Z], ...]
+
+  X = horizontal position
+  Y = vertical position
+  Z = magnitued
+
+  */
+  
+
+  var _areas = new Array();
+  var _conditions = new Array();
+  var _callbacks = new Array();
+
+  function initialize(areas, conditions, callbacks)
+  {
+    _areas = areas;
+    _conditions = conditions;
+    _callbacks = callbacks; 
+  }
+
+  function run(stream_data)
+  {
+    return activate_areas(stream_data, _areas);
+  }
+
+  function destroy()
+  {
+    _areas = new Array();
+    _conditions = new Array();
+    _callbacks = new Array();
+  }
+
+  function activate_areas(stream_data, areas)
+  {
+    active_areas = new Array();
+
+    for (i = 0; i < areas.length ; i++)
+    { 
+      x = stream_data[0];
+      y = stream_data[1];
+      
+      x_1 = areas[i][2][0];
+      x_2 = x_1 + areas[i][0];
+      
+      y_1 = areas[i][2][1];
+      y_2 = y_1 + areas[i][1];
+    
+      in_area = (y >= y_1) && (y <= y_2) && (x >= x_1) && (x <= x_2)
+    
+      if (in_area)
+      {
+        active_areas.push(i);
+        _callbacks[i]();
+      }
+    }
+  
+    return active_areas;
+  }
+  
+	return {
+		initialize: initialize,
+		run: run,
+    destroy: destroy
+	} 
+  
+}();
